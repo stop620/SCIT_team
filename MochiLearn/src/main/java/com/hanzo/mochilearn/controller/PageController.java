3e41c95b5cc651c29bd517e0ba40f179b9cc821b
Seungmin Seong <59172858+Koyuu77@users.noreply.github.com>
2025-08-20 12:32:19 +0900

    wordPage Fixed navigation Button Script Fixed wordCardPage Upload addWordCardPage Upload
    PageController Test API Upload

--- MochiLearn/src/main/java/com/hanzo/mochilearn/controller/PageController.java
+++ MochiLearn/src/main/java/com/hanzo/mochilearn/controller/PageController.java
@@ -2,6 +2,7 @@ package com.hanzo.mochilearn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,4 +28,19 @@ public class PageController {
     public String myPage() {
         return "page/myPage";
     }
+    
+	@GetMapping({"wordCard2","wordCard1"})
+	public String wordCard() {
+		return "page/wordCardPage";
+	}
+	
+	@GetMapping("addWordCard")
+	public String addWord() {
+		return "page/addWordCardPage";
+	}
+	
+	@PostMapping("addWordCard")
+	public String addWordCard() {
+		return "page/wordPage";
+	}
 }
