47c87f1a9d3f88bdf65ebdcfa9995fe30db041c6
eazyline <wltjs8627@gmail.com>
2025-08-25 12:21:38 +0900

    sts수정

--- MochiLearn/src/main/java/com/hanzo/mochilearn/dto/CardDTO.java
+++ MochiLearn/src/main/java/com/hanzo/mochilearn/dto/CardDTO.java
@@ -1,13 +1,17 @@
 package com.hanzo.mochilearn.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardDTO {
 
     private int id;
--- MochiLearn/src/main/java/com/hanzo/mochilearn/dto/SentenceDTO.java
+++ MochiLearn/src/main/java/com/hanzo/mochilearn/dto/SentenceDTO.java
@@ -1,9 +1,17 @@
 package com.hanzo.mochilearn.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SentenceDTO {
 
     private int id;
