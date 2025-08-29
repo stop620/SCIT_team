424f84ad6d95fd35b594811d7930688523fc33a1
Seungmin Seong <59172858+Koyuu77@users.noreply.github.com>
2025-08-29 17:01:25 +0900

    my page Graph scale / Y Axes Tick Update margin / padding Update

--- MochiLearn/src/main/resources/static/js/myPage.js
+++ MochiLearn/src/main/resources/static/js/myPage.js
@@ -5,90 +5,108 @@ document.addEventListener('DOMContentLoaded',()=>{
     var gradeTrend = document.getElementById('grade-trend');
     var etc = document.getElementById('etc');
 
-    const gradeTrendData = {
-        labels : ['0822','0823','0824','0825','0826','0827','0828'],
-        datasets : [{
-            backgroundColor : 'rgba(75, 192, 192, 1)',
-            borderColor : 'rgba(75, 192, 192, 1)',
-            label : '성적 통계',
-            fill : false,
-            data : [
-                5,4,6,7,8,9,7
-            ]
-        }],
-        options : {
-            maintainAspectRation : true,
-            title : {
-                text: '성적 추이'
-            },
-            scales : {
-                yAxes : [{
-                    scaleLabel : {
-                        display : true,
-                        labelString : '정답 수'
-                    }
-                }]
-            }
-        }
-    };
+    // const gradeTrendData = {
+    //     labels : ['0822','0823','0824','0825','0826','0827','0828'],
+    //     datasets : [{
+    //         backgroundColor : 'rgba(75, 192, 192, 1)',
+    //         borderColor : 'rgba(75, 192, 192, 1)',
+    //         label : '성적 통계',
+    //         fill : false,
+    //         data : [
+    //             5,4,6,7,8,9,7
+    //         ]
+    //     }]
+    // };
 
     const studyStatsChart = new Chart(studyStats,{
-    type:'doughnut',
-    data: {
-    labels: ['January', 'February', 'March', 'April', 'May'],
-    datasets: [
-            {
-                data: [50, 60, 70, 180, 190],
-            },
-        ],
-    }
+        type:'doughnut',
+        data: {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [{
+                        data: [50, 60, 70, 180, 190],
+                    },
+                ],
+            }
     });
 
     const studyTrackerChart = new Chart(studyTracker,{
         type:'doughnut',
         data: {
         labels: ['January', 'February', 'March', 'April', 'May'],
-        datasets: [
-                {
+        datasets: [{
                     data: [50, 60, 70, 180, 190],
-                },
-            ],
-        }
+                    },
+                ],
+            }
     });
 
     const gradeTrendChart = new Chart(gradeTrend,{
-        type : 'line',
-        data : {
-            labels : ['0822','0823','0824','0825','0826','0827','0828'],
-            datasets : [{
-                backgroundColor : 'rgba(75, 192, 192, 1)',
-                borderColor : 'rgba(75, 192, 192, 1)',
-                label : '성적 통계',
-                fill : false,
-                data : [
-                    5,4,6,7,8,9,7
-                ]
-            }],
+            type : 'line',
+            data : {
+                labels : ['0822','0823','0824','0825','0826','0827','0828'],
+                datasets : [{
+                    backgroundColor : 'rgba(75, 192, 192, 1)',
+                    borderColor : 'rgba(75, 192, 192, 1)',
+                    label : '성적 통계',
+                    fill : false,
+                    data : [
+                        5,4,6,7,8,9,7
+                    ]
+                }]
+            },
             options : {
-                maintainAspectRation : true,
+                maintainAspectRatio : true,
                 title : {
+                    display: true,
                     text: '성적 추이'
                 },
                 scales : {
-                    yAxes : [{
-                        scaleLabel : {
-                            display : true,
-                            labelString : '정답 수'
+                    y: {
+                        min : 0,
+                        max : 10,
+                        ticks: {
+                            stepSize :2
+                        },
+                    title : {
+                        display : true,
+                        text:'성적 추이'
                         }
-                    }]
+                    }
+                }
+            }
+        });
+
+    const etcChart = new Chart(etc, {
+        type : 'bar',
+        data : {
+            labels : ['초','중','고'],
+            datasets : [{
+                label : '난이도별 응시 횟수',
+                fill : false,
+                data : [2,4,8]
+            }],
+        },
+        options : {
+            maintainAspectRatio : true,
+            plugins: {
+                title : {
+                    display: true,
+                    text : '난이도별 응시 횟수'
+                }
+            },
+            scales : {
+                y: {
+                    min: 0,      // 최소값 0
+                    max: 10,     // 최대값 10
+                    ticks: {
+                        stepSize: 2
+                    },
+                    title: {
+                        display: true,
+                        text: '응시 횟수'
+                    }
                 }
             }
         }
     });
-
-    const etcChart = new Chart(etc,{
-        //그래프가 필요하면 추가 하기
-    });
-
 });
-
