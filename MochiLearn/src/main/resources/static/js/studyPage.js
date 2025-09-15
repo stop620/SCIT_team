5b754472e17d474fe7a11f84fbe00fa8694f08aa
eazyline <wltjs8627@gmail.com>
2025-09-15 09:18:36 +0900

    temp

--- MochiLearn/src/main/resources/static/js/studyPage.js
+++ MochiLearn/src/main/resources/static/js/studyPage.js
@@ -68,8 +68,13 @@ $(document).ready(function() {
 
             const cardHtml = `
                 <div class="card" onclick="location.href='/mochilearn/page/studyCard?cardId=${card.id}'">
-                    <img src="${thumbUrl}" alt="썸네일 이미지" />
-                    <h3>${card.title}</h3>
+					<div class="video-thumbnail">
+		                    <img src="${thumbUrl}" alt="썸네일 이미지" />
+							<div class="play-icon">▶</div>
+					</div>
+					<div class="video-info">
+                    <h3 class="video-title">${card.title}</h3>
+					
                     <div style="display:flex; justify-content:space-between; align-items:center; padding: 0 1rem;">
                         <span class="difficulty-tag ${difficultyClass}">${levelKor}</span>
                         <div style="color: #FFBB46; font-weight: 600;">⭐ ${card.like || 0}</div>
@@ -77,7 +82,8 @@ $(document).ready(function() {
                     <div class="tag-container" style="padding:0 1rem 1rem 1rem; display:flex; gap:0.5rem; flex-wrap:wrap;">
                         ${tagHtml}
                     </div>
-                </div>`;
+                </div>
+			</div>`;
 
             $('#cardGrid').append(cardHtml);
         });
